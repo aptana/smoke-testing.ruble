6566e5aa8ec9da2109d93f5833b8edf5d6995e16
Chris Williams <cwilliams@appcelerator.com>
2011-10-27 15:09:00 -0400

    update metadata

--- bundle.rb
+++ bundle.rb
@@ -3,17 +3,11 @@ require 'ruble'
 bundle do |bundle|
   bundle.display_name = 'smoke-testing'
   bundle.author = 'Christopher Williams'
-  bundle.copyright = <<END
-(c) Copyright 2011 appcelerator.com Distributed under MIT license.
-END
-
+  bundle.copyright = '(c) Copyright 2011 appcelerator.com Distributed under MIT license.'
   bundle.description = "Commands to help smoke test the IDE"
-
-  # uncomment with the url to the git repo if one exists
-  # bundle.repository = 'git@github.com:aptana/smoke-testing.ruble..git'
+  bundle.repository = 'git@github.com:aptana/smoke-testing.ruble.git'
 
   bundle.menu 'smoke-testing' do |menu|
     menu.command 'Validate Menu Entries'
-    menu.command 'This doesnt exist'
   end
 end
