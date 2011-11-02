d1ac21cb36b05c99d253718c627453cbe1eb8f6e
Ingo Muschenetz <ingo@muschenetz.com>
2011-11-02 14:33:44 -0700

    Changed bundle name

--- bundle.rb
+++ bundle.rb
@@ -1,13 +1,13 @@
 require 'ruble'
 
 bundle do |bundle|
-  bundle.display_name = 'smoke-testing'
+  bundle.display_name = 'Studio Smoke Testing'
   bundle.author = 'Christopher Williams'
   bundle.copyright = '(c) Copyright 2011 appcelerator.com Distributed under MIT license.'
   bundle.description = "Commands to help smoke test the IDE"
   bundle.repository = 'git@github.com:aptana/smoke-testing.ruble.git'
 
-  bundle.menu 'smoke-testing' do |menu|
+  bundle.menu 'Studio Smoke Testing' do |menu|
     menu.command 'Validate Menu Entries'
   end
 end
