832c18dd8314e89dee68f1e4a885f97feffaa1ea
Ingo Muschenetz <ingo@muschenetz.com>
2012-03-19 15:03:54 -0700

    Corrected repo URL to use read-only version.

--- bundle.rb
+++ bundle.rb
@@ -5,7 +5,7 @@ bundle do |bundle|
   bundle.author = 'Christopher Williams'
   bundle.copyright = '(c) Copyright 2011 appcelerator.com Distributed under MIT license.'
   bundle.description = "Commands to help smoke test the IDE"
-  bundle.repository = 'git@github.com:aptana/smoke-testing.ruble.git'
+  bundle.repository = 'git://github.com/aptana/smoke-testing.ruble.git'
 
   bundle.menu 'Studio Smoke Testing' do |menu|
     menu.command 'Validate Menu Entries'
